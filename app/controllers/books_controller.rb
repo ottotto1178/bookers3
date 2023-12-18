dc32a632140444b0a53c7fe99f05a7440ed43249
EC2 Default User <ec2-user@ip-172-31-12-236.ap-northeast-1.compute.internal>
2023-12-18 04:43:56 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,55 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+  
+  def create
+    @book = Book.new(book_param)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+    user = @book.user
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_param)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  private
+  
+  def book_param
+    params.require(:book).permit(:title, :body)
+  end
+end
